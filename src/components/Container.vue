9f977be874cadbd97a67b21a14fd1539f2ae138e
CHUNDOOIN <chun.doo.in@gmail.com>
2022-08-08 16:51:32 +0900

    22.08.08 vue 강의

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <Post v-for="(myData, i) in myData" :key="i" :myData="myData" />
+        <!-- 
+        <Post :myData="myData[0]" />
+        <Post :myData="myData[1]" />
+        <Post :myData="myData[2]" /> -->
+    </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+    name: "Container",
+    props: {
+        myData: Array,
+    },
+    components: { Post },
+    // mounted() {
+    //     console.log(this.myData);
+    // },
+};
+</script>
+<style></style>
